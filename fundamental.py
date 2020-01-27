28181947b485aacb89564c0e4be91d011b3c2228
rochafialvin <rochafi.dev@gmail.com>
2020-01-27 16:38:50 +0700

    first commit

---
+++ fundamental.py
@@ -0,0 +1,42 @@
+# Untuk menampilkan teks (komentar)
+#print('My first code')
+
+# Variable
+# Untuk menyimpan sebuah data
+# Tidak bisa diawali dengan angka
+# string, tipe data yg menyimpan teks = 'Vinales'
+# number , float : desimal (0.25, 3.14), integer : bulat (3, 5, 6)
+# camelCase
+# firstName = 'Vinales'
+# lastName = 'John'
+# age = 32
+
+# Function type digunakan untuk mengethaui tipe data
+# dari sebuah variable
+# result = type(age)
+# print(result)
+
+# print(type(age))
+
+# function input digunakan untuk menerima inputan user
+name = input("Siapa Nama Anda : ")
+day = input("Hari apakah saat ini : ")
+
+print('Nama saya adalah  ' + name)
+print('Hari ini adalah hari ' + day)
+
+
+
+
+
+
+
+
+
+
+
+
+# Duplicate code : SHIFT + ALT + DOWN ARROW
+# Untuk membersihkan terminal : CTRL + L
+# Membuat komentar : CTRL + /
+# Membuka terminal : CTRL + J
