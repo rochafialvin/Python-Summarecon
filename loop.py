82cff738573ec199e504ea3a596e2536b8f13a08
rochafialvin <rochafi.dev@gmail.com>
2020-01-30 14:48:00 +0700

    While and For

---
+++ loop.py
@@ -0,0 +1,35 @@
+# LOOP
+
+# WHILE LOOP
+# number = 24
+
+# Akan terus berjalan selama
+# kondisi pada while bernilai true
+# while number <= 10:
+#     print(f'Genap : {number}')
+#     number += 2
+
+# FOR LOOP
+
+# [0, 1, 2, 3, 4, 5]
+# for i in range(6) :
+#     print(f'Angka : {i}')
+
+# [0, 2, 4, 6, 8]
+for i in range(0, 9, 2):
+    print(f'G E N A P : {i}')
+
+# [1, 3, 5, 7, 9]
+for i in range(1, 10, 2):
+    print(f'G A N J I L : {i}')
+
+# [0, 1, 2, 3, 4]
+# range(0, 5)
+# range(5)
+
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# range(1, 11)
+
+# [1, 3, 5, 7, 9]
+# range(1, 11, 2)
+
