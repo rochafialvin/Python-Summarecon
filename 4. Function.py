03f6f633b400684c8b240942e6f2baa47d4bdfff
rochafialvin <rochafi.dev@gmail.com>
2020-01-31 16:29:16 +0700

    pi wikend

--- 4. Function.py
+++ 4. Function.py
@@ -59,26 +59,38 @@ day3 = 'Tuesday'
 # Append & pop
 
 # Append untuk menambahkan data ke list
-fruits = ['Apple', 'Banana', 'Cherry']
-fruits.append('Durian')
+# fruits = ['Apple', 'Banana', 'Cherry']
+# fruits.append('Durian')
 
-print(fruits)
+# print(fruits)
 
 # Pop akan mengeluarkan data dari list
 # Kita bisa menentukan data yang ingin di pop dengan index
-fruits.pop() # Durian
-fruits.pop(0) # Apple
+# fruits.pop() # Durian
+# fruits.pop(0) # Apple
 
-print(fruits)
+# print(fruits)
 
-fruits[0] = 'Pisang'
-fruits[1] = 'Grape'
+# fruits[0] = 'Pisang'
+# fruits[1] = 'Grape'
 
-print(fruits)
+# print(fruits)
 
 
+listList = [1, "String", True,['Satu', 'Dua', 'Tiga']]
+number = listList[3]
+print(number[1])
+
+# listList[3][1]
+
+# PERTAMA
 # Buat sebuah function yang menerima list
 # Akan me return hasil kali dua dari semua angka di dalam list
 
 # nilaiAWal = [1, 3, 5, 7]
 # hasilakhir = [2, 6, 10, 14]
+
+# KEDUA
+# Sebuah function yang dapat memisahkan nilai genap dan ganjil
+# [11, 22, 34, 41, 52, 63, 71, 86,]
+# [[22, 34, 52, 86], [11, 41, 63 ,71]]
