3aa89a3e19218caa20052181d188a6ebcbf434ab
rochafialvin <rochafi.dev@gmail.com>
2020-02-04 15:31:56 +0700

    map filter

---
+++ 5. Lambda, Map, filter.py
@@ -0,0 +1,61 @@
+# def times2(num):
+#     return num * 2
+
+# result = times2(4) # 2
+
+# Lambda
+# Biasa digunakan sebagai callback function
+
+# lambda num: num * 2
+
+# MAP
+# Digunakan untuk merubah bentuk data
+# Menerima dua parameter , function dan list
+# function yang memiliki istilah 'callback function'
+# function ini harus menerima satu buah parameter
+# function tersebut harus me return sesuatu
+# return mapobject, untuk mendapatkan listnya harus
+# di masukkan ke dalam function list()
+
+# witch function
+# def times2(num):
+#     return num * 2
+
+# numList = [1, 2, 3 ,4 , 5]
+
+# mapObj = map(times2, numList)
+# result = list(mapObj)
+
+# print(result)
+
+# with lambda
+
+# mapObj = map(lambda num: num * 2, numList)
+# result = list(mapObj)
+
+# print(result)
+
+
+# FILTER
+# Digunakan untuk menyaring data
+# Menerima dua parameter , function dan list
+# function yang memiliki istilah 'callback function'
+# function ini harus menerima satu buah parameter
+# function tersebut harus me return bool (True , False)
+# return filterobject, untuk mendapatkan listnya harus
+# di masukkan ke dalam function list()
+
+# with function
+def even(num):
+    return num % 2 == 0
+
+numList = [11, 12, 13 ,14 , 15, 16, 17, 18, 19, 20]
+
+evenList = list(filter(even, numList))
+
+print(evenList)
+
+# With Lambda
+evenList = list(filter(lambda x: x % 2 == 0, numList))
+
+print(evenList)
--- SOLVE-IT/market.py
+++ SOLVE-IT/market.py
@@ -1,71 +1,72 @@
 fruits = ['Apel', 'Anggur', 'Jeruk']
 stock = [5, 7, 8]
 price = [1000, 15000, 20000]
+qty = [0 , 0, 0]
 
-askAgain = True
-while askAgain:
-    # User akan menginput jumlah qty buah yg diinginkan
-    qtyApple =  int(input('\nMasukkan jumlah Apel : '))
-    # Jika permintaan user melebihi stock
-    if qtyApple > stockApple:
-        print(f'Kesalahan input, stock Apel : {stockApple}')
-    else :
-        askAgain = False
+# input dari user diubah ke integer
+mainOpt = int(input(
+    'Menu Utama \n' +
+    '1. Menampilkan list\n' +
+    '2. Menambahkan buah\n' +
+    '3. Belanja\n' +
+    'Menu pilihan :' 
+))
+
+if mainOpt == 1:
+    print('CreateList')
+elif mainOpt == 2 :
+    print('Add Fruit')
+elif mainOpt == 3 :
+    print('Shopping')
+
+    # Akan loop sebanyak data buah
+    for i in range(len(fruits)):
+        askAgain = True
+        while askAgain:
+            # User akan menginput jumlah qty buah yg diinginkan
+            qty[i] =  int(input(f'\nMasukkan jumlah {fruits[i]} : '))
+            # Jika permintaan user melebihi stock
+            if qty[i] > stock[i]:
+                print(f'Kesalahan input, stock Apel : {stock[i]}')
+            else :
+                askAgain = False
 
 
-askAgain = True
-while askAgain :
-    # User akan menginput jumlah qty buah yg diinginkan
-    qtyGrape =  int(input('\nMasukkan jumlah Anggur : '))
-    # Jika permintaan user melebihi stock
-    if qtyGrape > stockGrape:
-        print(f'Kesalahan input, stock Anggur : {stockGrape}')
-    else :
-        askAgain = False
 
 
 
-askAgain = True
-while askAgain :
-    # User akan menginput jumlah qty buah yg diinginkan
-    qtyOrange =  int(input('\nMasukkan jumlah Jeruk : '))
-    # Jika permintaan user melebihi stock
-    if qtyOrange > stockOrange:
-        print(f'Kesalahan input, stock Jeruk : {stockOrange}')
-    else :
-        askAgain = False
 
-# Hitung total harga setiap buah
-totalApple = qtyApple * priceApple
-totalGrape = qtyGrape * priceGrape
-totalOrange = qtyOrange * priceOrange
+    # # Hitung total harga setiap buah
+    totalApple = qtyApple * priceApple
+    totalGrape = qtyGrape * priceGrape
+    totalOrange = qtyOrange * priceOrange
 
-# Hitung total belanja keseluruhan
-totalPrice = totalApple + totalGrape + totalOrange
+    # # Hitung total belanja keseluruhan
+    totalPrice = totalApple + totalGrape + totalOrange
 
-print(
-    '\n# # # # # # # # # # # # # # # # # #\n' +
-    'Detail Belanja \n\n' +
-    f'Apel : {qtyApple} x {priceApple} = {totalApple}\n' +
-    f'Anggur : {qtyGrape} x {priceGrape} = {totalGrape}\n' +
-    f'Jeruk : {qtyOrange} x {priceOrange} = {totalOrange}\n\n' +
-    f'Total : {totalPrice}' +
-    '\n# # # # # # # # # # # # # # # # # # '
-)
+    print(
+        '\n# # # # # # # # # # # # # # # # # #\n' +
+        'Detail Belanja \n\n' +
+        f'Apel : {qtyApple} x {priceApple} = {totalApple}\n' +
+        f'Anggur : {qtyGrape} x {priceGrape} = {totalGrape}\n' +
+        f'Jeruk : {qtyOrange} x {priceOrange} = {totalOrange}\n\n' +
+        f'Total : {totalPrice}' +
+        '\n# # # # # # # # # # # # # # # # # # '
+    )
 
-moneyAgain = True
+    # moneyAgain = True
 
-while moneyAgain:
-    # User akan input uang
-    userMoney = int(input('\nMasukkan jumlah uang : '))
-    # Cari selisih uang user dengan total belanja
-    margin = userMoney - totalPrice
-    # Jika uang yang user masukkan kurang
-    if userMoney < totalPrice:
-        print(f'Uang yang Anda masukkan kurang Rp.{margin}')
-    else :
-        print(f'Terimakasih, uang kembalian Anda Rp. {margin}')
-        moneyAgain = False
+    # while moneyAgain:
+    #     # User akan input uang
+    #     userMoney = int(input('\nMasukkan jumlah uang : '))
+    #     # Cari selisih uang user dengan total belanja
+    #     margin = userMoney - totalPrice
+    #     # Jika uang yang user masukkan kurang
+    #     if userMoney < totalPrice:
+    #         print(f'Uang yang Anda masukkan kurang Rp.{margin}')
+    #     else :
+    #         print(f'Terimakasih, uang kembalian Anda Rp. {margin}')
+    #         moneyAgain = False
 
 
 # Upgrade :
