f53f2589ebe1a5195f27e07a0235355b30a8b2ba
rochafialvin <rochafi.dev@gmail.com>
2020-02-07 16:16:09 +0700

    git push origin master

---
+++ exercise.py
@@ -0,0 +1,18 @@
+# Number One
+# def wordRev
+# Buatlah sebuah function yang menerima sebuah kata
+# Reverse huruf setiap kata
+
+# Contoh:
+#   Hello my friend --> olleH ym dneirf
+#   abc de efg --> cba ed gfe
+
+
+# Number Two
+# def sum01
+# Buat function yang menerima list of number
+# Jumlahkan semua angka, kecuali angka yang ada diantara angka 0 - 1
+
+# [2, 4, 0, 1, 11] -- > 17 (0, 1 tidak masuk hitungan)
+# [7, 0, 3, 1, 7, 9] --> 23 (0, 3, 1 tidak masuk hitungan)
+# [5, 0, 3, 2, 1] --> 5 (0, 3, 2, 1 tidak masuk hitungan)
