ffa675af9439e27f1c09a4c647c71b87ec7ad0c0
rochafialvin <rochafi.dev@gmail.com>
2020-02-17 14:04:50 +0700

    mult

--- Review/rev-exercise.py
+++ Review/rev-exercise.py
@@ -33,6 +33,45 @@ no_vowel('Today is friday')
 # 4 -> nol kali
 ####################################################################
 
+def mult(numb):
+    # numb : 399
+    numb = str(numb)
+
+    # numb = '399'
+    listInt = []
+    for i in numb: # '3', '9', '9'
+        listInt.append(int(i))
+
+    #listInt = [3, 9, 9]
+
+    count = 0
+    while len(listInt) > 1:
+        # Mengalikan semua angka pada list
+        res = 1
+        for i in listInt: # listInt = [3, 9, 9]
+            res *= i 
+
+        # res = 8
+        
+        # Hasil akhir dari perkalian akan diubah menjadi list lagi
+        # Untuk kemudian dilakukan perkalian seperti di atas
+        res = str(res)
+        # res = '243'
+        listInt = []
+        for i in res:
+            listInt.append(int(i))
+
+        # listInt = [8]
+
+        count += 1
+
+    return count
+
+
+print('Banyaknya perkalian : ', mult(8))
+
+
+
 ##################################################################
 # tentukan apakah sebuah kata memiliki perulangan huruf atau tidak
 # buatlah function untuk mendeteksi itu
